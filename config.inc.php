b2dead1dac81d83153e66003efffd4ed22883358
GauravSolo <gauravsharma9339@gmail.com>
2024-12-01 02:14:13 +0530

    create env file

--- config.inc.php
+++ config.inc.php
@@ -1,17 +1,29 @@
 <?php
+if (file_exists(__DIR__ . '/.env')) {
+  require_once __DIR__ . '/vendor/autoload.php';
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  
+  $dotenv->load();
+  
+  putenv("DBNAME=" . $_ENV['DBNAME']);
+  putenv("DBUSER=" . $_ENV['DBUSER']);
+  putenv("DBPASS=" . $_ENV['DBPASS']);
+  putenv("DBHOST=" . $_ENV['DBHOST']);
+}
+
 
 date_default_timezone_set('Asia/Kolkata');
 session_start();
 
+define('DBNAME', getenv('DBNAME'));
+define('DBUSER', getenv('DBUSER'));
+define('DBPASS', getenv('DBPASS'));
+define('DBHOST', getenv('DBHOST'));
 
-define('DBNAME',getenv('DBNAME'));
-define('DBUSER',getenv('DBUSER'));
-define('DBPASS',getenv('DBPASS'));
-define('DBHOST',getenv('DBHOST'));
 try {
-  $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-  echo "Issue -> Connection failed: " . $e->getMessage();
+    $db = new PDO("mysql:host=" . DBHOST . ";dbname=" . DBNAME, DBUSER, DBPASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Issue -> Connection failed: " . $e->getMessage();
 }
 ?>
