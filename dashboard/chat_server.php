fb484d3b8cb6eff317006b92486bfa8e5f70a91b
GauravSolo <gauravsharma9339@gmail.com>
2024-11-30 23:28:39 +0530

    Fix config.inc.php path

--- dashboard/chat_server.php
+++ dashboard/chat_server.php
@@ -16,7 +16,7 @@ class Chat implements MessageComponentInterface {
         $this->clients = [];
 
         // Initialize Database connection
-        require '/config.inc.php';  // Assuming this file contains your DB connection
+        require require __DIR__ . '/../config.inc.php';
         $this->ch_db = $db;  // Assuming $db is your PDO connection
     }
 
